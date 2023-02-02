3594025415856a8d57aef4a2dd281eff1f298c45
Guido Maciocci <guidomaciocci@gmail.com>
2023-02-02 08:39:53 +0100

    add logging

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const actions = require('@actions/core');
 const { google } = require('googleapis');
 const fs = require('fs');
 const archiver = require('archiver');
+const { file } = require('googleapis/build/src/apis/file');
 
 /** Google Service Account credentials  encoded in base64 */
 const credentials = actions.getInput('credentials', { required: true });
 /** Google Drive Folder ID to upload the file/folder to */
 const folder = actions.getInput('folder', { required: false });
-actions.info('folder', folder)
+actions.info(`folder ${folder}`)
 /** Local path to the file/folder to upload */
 const target = actions.getInput('target', { required: true });
 /** Link to the Drive folder */
 const link = 'link';
 
 const credentialsJSON = JSON.parse(Buffer.from(credentials, 'base64').toString());
-actions.info('credentialsJSON', credentialsJSON.toString())
+actions.info(`credentialsJSON ${credentialsJSON}`)
              
 const scopes = ['https://www.googleapis.com/auth/drive'];
 const auth = new google.auth.JWT(credentialsJSON.client_email, null, credentialsJSON.private_key, scopes);
@@ -22,7 +23,7 @@ const drive = google.drive({ version: 'v3', auth });
 
 const driveLink = `https://drive.google.com/drive/folders/${folder}`
 let filename = target.split('\\').pop();
-actions.info('filename', filename.toString());
+actions.info(`filename ${filename}`);
 
 async function main() {
   //actions.setOutput(link, driveLink);
@@ -41,7 +42,7 @@ function uploadToDrive() {
    actions.info('body', body.toString())
   } 
   catch (e) {
-    actions.error('Something wrong creating body');
+    actions.error('Something wrong in creating body...');
     throw e;
   }
   
@@ -55,9 +56,9 @@ function uploadToDrive() {
     },
     supportsAllDrives: true,
     supportsTeamDrives: true
-  }).then(() => actions.info('File uploaded successfully'))
+  }).then(() => actions.info('File uploaded successfully...'))
     .catch(e => {
-      actions.error('Upload failed');
+      actions.error('Upload failed!');
       throw e;
     });
 }
